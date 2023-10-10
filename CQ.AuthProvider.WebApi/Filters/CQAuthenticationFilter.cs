d8a27122ace3b1213e7c484fda937ddd3378de2c
Daniel Acevedo <daniel18acevedo@gmail.com>
2023-10-10 13:01:01 -0300

    Adding authentication filter

--- CQ.AuthProvider.WebApi/Filters/CQAuthenticationFilter.cs
+++ CQ.AuthProvider.WebApi/Filters/CQAuthenticationFilter.cs
@@ -1,4 +1,4 @@
-﻿// using CQ.ApiFilters.Core;
+﻿using CQ.ApiElements.Filters;
 using dotenv.net.Utilities;
 using Microsoft.AspNetCore.DataProtection.KeyManagement;
 using Microsoft.AspNetCore.Mvc.Filters;
@@ -9,37 +9,37 @@ using System.Text;
 
 namespace PlayerFinder.Auth.Api.Filters
 {
-    //internal class CQAuthenticationFilter : AuthenticationHandlerFilter
-    //{
-    //    protected override bool IsFormatOfTokenValid(string token)
-    //    {
-    //        try
-    //        {
-    //            var tokenHandler = new JwtSecurityTokenHandler();
-    //            var validationParameters = GetValidationParameters();
+    internal class CQAuthenticationFilter : AuthenticationFilter
+    {
+        protected override bool IsFormatOfTokenValid(string token)
+        {
+            try
+            {
+                var tokenHandler = new JwtSecurityTokenHandler();
+                var validationParameters = GetValidationParameters();
 
-    //            SecurityToken validatedToken;
-    //            IPrincipal principal = tokenHandler.ValidateToken(token, validationParameters, out validatedToken);
-    //            return true;
-    //        }
-    //        catch (Exception)
-    //        {
-    //            return false;
-    //        }
-    //    }
+                SecurityToken validatedToken;
+                IPrincipal principal = tokenHandler.ValidateToken(token, validationParameters, out validatedToken);
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
 
-    //    private TokenValidationParameters GetValidationParameters()
-    //    {
-    //        var firebasePrivateKey = Environment.GetEnvironmentVariable("firebase:private-key");
-    //                                return new TokenValidationParameters()
-    //        {
-    //            ValidateLifetime = false, // Because there is no expiration in the generated token
-    //            ValidateAudience = false, // Because there is no audiance in the generated token
-    //            ValidateIssuer = false,   // Because there is no issuer in the generated token
-    //            ValidIssuer = "Sample",
-    //            ValidAudience = "Sample",
-    //            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(firebasePrivateKey)) // The same key as the one that generate the token
-    //        };
-    //    }
-    //}
+        private TokenValidationParameters GetValidationParameters()
+        {
+            var firebasePrivateKey = Environment.GetEnvironmentVariable("firebase:private-key");
+            return new TokenValidationParameters()
+            {
+                ValidateLifetime = false, // Because there is no expiration in the generated token
+                ValidateAudience = false, // Because there is no audiance in the generated token
+                ValidateIssuer = false,   // Because there is no issuer in the generated token
+                ValidIssuer = "Sample",
+                ValidAudience = "Sample",
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(firebasePrivateKey)) // The same key as the one that generate the token
+            };
+        }
+    }
 }
